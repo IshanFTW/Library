66ad0d0f478af1e1f3ee413af296dfb510f8b30d
Ishan Jindal <ishanjindal2003@gmail.com>
2023-09-09 02:27:00 +0530

    set up a table to display books

--- script.js
+++ script.js
@@ -18,10 +18,27 @@ form.addEventListener('submit', function(event){
 
     const newBook = new book(title, author, pages, read);
     addBookToLibrary(newBook);
+    displayBooks();
 });
 
 function addBookToLibrary(book){
     myLibrary.push(book);
+}
+
+function displayBooks(){
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+
+    myLibrary.forEach(function(book, index){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}<td>
+        <td>${book.author}<td>
+        <td>${book.pages}<td>
+        <td>${book.read ? 'yes' : 'No'}<td>
+        `;
+        bookList.appendChild(row);
+    });
 }
 
 
